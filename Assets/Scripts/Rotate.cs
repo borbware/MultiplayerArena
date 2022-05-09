795d7551414ebe14fba5b4a0961b68c7b1e6c3f3
borbware <perttu.tuovinen@gmail.com>
2022-05-09 11:26:27 +0300

    update to .32, add rotate behavior back

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -8,5 +8,6 @@ public class Rotate : MonoBehaviour
     {
 		if (StageManager.instance.stageState != "play")
             return;
+        transform.Rotate(new Vector3(0,rotationSpeed * Time.deltaTime,0));
     }
 }
