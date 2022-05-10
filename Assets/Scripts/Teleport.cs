9b3f2fe6904163862e32553f8ee06b53a36b9596
Noora Heinsuo <noora.heinsuo@gmail.com>
2022-05-10 13:02:07 +0300

    Add Teleporters

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collider)
+    {
+        var obj = collider.gameObject;
+        if (obj.tag == "Player" && obj.GetComponent<Player>().state == "active")
+        {
+            Rigidbody rb = obj.GetComponent<Rigidbody>();
+            rb.MovePosition(new Vector3(
+                Random.Range(3.5f, -3.5f),
+                0.5f,
+                Random.Range(3.5f, -3.5f)
+            ));
+
+            Destroy(gameObject);
+        }
+    }
+}
