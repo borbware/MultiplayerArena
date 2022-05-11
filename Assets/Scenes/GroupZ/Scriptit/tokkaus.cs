3b1448eb539e844f5212c4958175c437fe0b7a31
Janneskulio <>
2022-05-11 10:39:42 +0300

    audio script

--- Assets/Scenes/GroupZ/Scriptit/tokkaus.cs
+++ Assets/Scenes/GroupZ/Scriptit/tokkaus.cs
@@ -7,24 +7,34 @@ public class tokkaus : MonoBehaviour
     float pushforce = 7;
     Player player;
     Vector3 spawnlocation;
-    [SerializeField] AudioClip slap;
+    AudioSource slap,walkingdownthestreet;
+    Rigidbody tisrigid;
+    float audiocd = 0;
     void Start()
     {
+        walkingdownthestreet = GetComponent<AudioSource>();
         spawnlocation = transform.position;
         player = GetComponent<Player>();
+        slap = GetComponent<AudioSource>();
+        tisrigid = gameObject.GetComponent<Rigidbody>();
     }
-
-    
     void Update()
     {
+        audiocd -= 1 * Time.deltaTime;
+        if(tisrigid.velocity.sqrMagnitude > 1.4f)
+        {
+            if(audiocd < 0)
+            {walkingdownthestreet.Play(0); audiocd = 1;}
+        }
         
     }
     void OnTriggerEnter(Collider other) 
     {   
-        var tisrigid = gameObject.GetComponent<Rigidbody>();
+     
         GameObject toher = other.gameObject;
         if(toher.tag == "Player")
         {
+            slap.Play(0);
             var pushother = toher.GetComponent<Rigidbody>();
             pushother.AddForce((toher.transform.position - gameObject.transform.position)
             .normalized * tisrigid.velocity.sqrMagnitude * pushforce);
