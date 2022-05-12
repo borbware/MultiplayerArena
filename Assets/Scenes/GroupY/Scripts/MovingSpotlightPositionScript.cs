8c77bae2e23d0bbec584fe027825d0ca984ddb95
Petri <zenid@windowslive.com>
2022-05-12 12:26:02 +0300

    Changed name of MovingSpotlight to MovingSpotlightPrefab and made an Instantiate function in MovingSpotlightPositionScript.

--- Assets/Scenes/GroupY/Scripts/MovingSpotlightPositionScript.cs
+++ Assets/Scenes/GroupY/Scripts/MovingSpotlightPositionScript.cs
@@ -4,28 +4,33 @@ using UnityEngine;
 
 public class MovingSpotlightPositionScript : MonoBehaviour
 {
+    public GameObject movingSpotlightPrefab;
+    public Vector3 initialPositionOfLight = new Vector3(0f, 8.5f, 0f);
 
-    
-    private Vector3 InitialPositionOfLight;
-    private Vector3 positionDisplacement;
-
-    // float moveSpeed = 0.01f;
 
     // Start is called before the first frame update
     void Start()
     {
-
-        InitialPositionOfLight = transform.position;
-        float randomDistance = Random.Range(5f, -5f);
-        positionDisplacement = new Vector3(randomDistance, randomDistance, randomDistance);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        StageManager.instance.stageTime += Time.deltaTime;
-        transform.position = Vector3.Lerp(InitialPositionOfLight, InitialPositionOfLight + positionDisplacement, Mathf.PingPong(StageManager.instance.stageTime, 2));
+
+        if (StageManager.instance.stageState == StageManager.StageState.Play && StageManager.instance.stageTime == 115f) {
+
+            SpotLightSpawn();
+
+        }
+        
+    }
+
+    void SpotLightSpawn() 
+    {
+
+            Instantiate(movingSpotlightPrefab, initialPositionOfLight, Quaternion.identity);
+ 
     }
 
 }
