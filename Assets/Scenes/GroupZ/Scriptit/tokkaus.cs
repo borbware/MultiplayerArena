dfb3a7963b252310ad7cf0d719351516a431de60
Janneskulio <>
2022-05-12 11:07:59 +0300

    working push and player updates

--- Assets/Scenes/GroupZ/Scriptit/tokkaus.cs
+++ Assets/Scenes/GroupZ/Scriptit/tokkaus.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class tokkaus : MonoBehaviour
 {
-    float pushforce = 7;
+    float pushforce = 100;
     Player player;
     Vector3 spawnlocation;
     AudioSource walking;
     [SerializeField] AudioClip slap;
     Rigidbody tisrigid;
     float maxVel = 12f;
+    float liftUp = 25f;
     [SerializeField] float audiocd = 0, dashspeed = 5;
+    bool canDash = true;
     void Start()
     {
         spawnlocation = transform.position;
@@ -29,28 +31,44 @@ public class tokkaus : MonoBehaviour
             
         }
         else{walking.Stop();}
-        if(player.GetComponent<Player>().shootInput)
+        if(player.GetComponent<Player>().shootInput && canDash == true)
         {
-            Debug.Log("podwq");
-            tisrigid.AddForce(transform.forward * dashspeed);
+            StartCoroutine(DashCooldown());
+        }
+        else if (player.GetComponent<Player>().shootInput && canDash == false)
+        {
+            Debug.Log("Can't dash");
         }
 
         if(tisrigid.velocity.magnitude > maxVel)
-            tisrigid.velocity = tisrigid.velocity.normalized * maxVel;
-
-       
-        
+            tisrigid.velocity = tisrigid.velocity.normalized * maxVel; 
     }
-    void OnTriggerEnter(Collider other) 
+    IEnumerator DashCooldown()
+    {
+
+        canDash = true;
+        yield return new WaitForSeconds(0.01f);
+        tisrigid.AddForce(transform.up * liftUp);
+        tisrigid.AddForce(transform.forward * dashspeed);
+        canDash = false;
+        if(tisrigid.velocity.magnitude > maxVel)
+            tisrigid.velocity = tisrigid.velocity.normalized * maxVel;
+        yield return new WaitForSeconds(2f);
+        canDash = true;
+    }
+    void OnCollisionEnter(Collision other) 
     {   
      
         GameObject toher = other.gameObject;
         if(toher.tag == "Player")
         {
+            Debug.Log("triggering");
             walking.PlayOneShot(slap);
             var pushother = toher.GetComponent<Rigidbody>();
-            pushother.AddForce((toher.transform.position - gameObject.transform.position)
-            .normalized * tisrigid.velocity.sqrMagnitude * pushforce);
+            var dist = toher.transform.position - gameObject.transform.position;
+            var dotproduct = Vector3.Dot(tisrigid.velocity, dist)/dist.magnitude;
+            if (dotproduct > 0)
+                pushother.AddForce(dist.normalized * dotproduct * pushforce);
         }    
         if(toher.tag == "fial")
         {
