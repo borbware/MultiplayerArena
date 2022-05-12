33a69a0bee97c0036f1f953bd5c2743e872894f2
myUserName <jovirta96@gmail.com>
2022-05-12 12:34:34 +0300

    Bllet Knockback

--- Assets/Scenes/GroupY/Scripts/BulletCollider.cs
+++ Assets/Scenes/GroupY/Scripts/BulletCollider.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class BulletCollider : MonoBehaviour
 {
+    Rigidbody rb;
+    Rigidbody destRB;
+
+    Vector3 KnockbackDir;
+    void Start(){
+        rb = gameObject.GetComponent<Rigidbody>();
+        KnockbackDir = rb.transform.forward;
+        //newBullet.GetComponent<Rigidbody>().AddForce(
+                    //transform.forward * shootForce * Time.fixedDeltaTime);
+    }
+    [SerializeField] float damage = 10f;
     void OnCollisionEnter(Collision C){
         Debug.Log(C.gameObject.tag);
+        C.gameObject.SendMessage("Hurt", damage, SendMessageOptions.DontRequireReceiver);
         if(C.gameObject.tag == "Player")
+        C.gameObject.GetComponent<Rigidbody>().AddForce(
+        KnockbackDir * 5);
             Destroy(gameObject);
     }
 }
---
+++ Assets/Scenes/GroupY/Scripts/PlayerKnockback.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerKnockback : MonoBehaviour
+{
+    bool KnockedBack;
+
+    Rigidbody rb;
+    Rigidbody destRB;
+    Vector3 KnockbackDir;
+    // Start is called before the first frame update
+    void Start()
+    {
+        destRB = gameObject.GetComponent<Rigidbody>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate(){
+        if (KnockedBack == true){
+            destRB.gameObject.GetComponent<Rigidbody>().AddForce(
+        KnockbackDir * 1000 * Time.fixedDeltaTime);
+        }
+    }
+
+    
+    void OnCollisionEnter(Collision C){
+        Debug.Log(C.gameObject.tag);
+        if(C.gameObject.tag == "Projectile" && KnockedBack == false){
+            rb = C.gameObject.GetComponent<Rigidbody>();
+            KnockbackDir = rb.transform.forward;
+            KnockedBack = true;
+            Invoke("CancelKnockback", 0.2f);
+        }
+    }
+
+    void CancelKnockback(){
+        KnockedBack = false;
+    }
+}
