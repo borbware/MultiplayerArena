1254ae01c999384937252220a192fd12773525cd
myUserName <jovirta96@gmail.com>
2022-05-13 12:38:07 +0300

    Added powerup spawner for autofire

--- Assets/Scenes/GroupY/Scripts/PowerUpSpawner.cs
+++ Assets/Scenes/GroupY/Scripts/PowerUpSpawner.cs
@@ -5,15 +5,37 @@ using UnityEngine;
 public class PowerUpSpawner : MonoBehaviour
 {
     [SerializeField] GameObject PowerUp;
-    // Start is called before the first frame update
+
+    float Spawnrate = 15f;
+
+    float LastSpawn = 0f;
+    GameObject SpawnedPowerUp;
+
+    Vector3 SpawnPoint;
+
+    int RandomSpawn;
+
+    GameObject[] obj;
+
     void Start()
     {
-        
+        obj = GameObject.FindGameObjectsWithTag("Platform");
+        LastSpawn = Time.time;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(Time.time > LastSpawn + Spawnrate){
+            LastSpawn = Time.time;
+            SpawnPowerUp();
+        }
+    }
+
+    void SpawnPowerUp(){
+        RandomSpawn = Random.Range(0, obj.Length);
+        SpawnPoint = new Vector3
+        (obj[RandomSpawn].transform.position.x, 40f, obj[RandomSpawn].transform.position.z);
+        SpawnedPowerUp = Instantiate(PowerUp,SpawnPoint,Quaternion.identity);
+        Destroy(SpawnedPowerUp, 30);
     }
 }
--- Assets/Scenes/GroupY/Scripts/Shoot.cs
+++ Assets/Scenes/GroupY/Scripts/Shoot.cs
@@ -26,15 +26,16 @@ public class Shoot : MonoBehaviour
     float ChargeStart;
     float LastChargePress = 0;
 
+    bool CanPickUp;
+
     GameObject scoreObj;
     Text scoreText;
 
     private void Start()
     {
         UIManager = StageManager.instance.UIManagers[PlayerIndex - 1];
-        UIManager.AddScore(50);
         _player = GetComponent<Player>();
-
+        CanPickUp = true;
         //scoreObj = transform.Find("Score").gameObject;
         //scoreText = scoreObj.GetComponent<Text>();
     }
@@ -85,6 +86,7 @@ public class Shoot : MonoBehaviour
                 Luoti,
                 transform.position + transform.forward * LuotiSpeed,
                 Quaternion.identity);
+            if(DeductScore == true){UIManager.AddScore(-1);}
             Destroy(newBullet, 5);
             newBullet.GetComponent<Rigidbody>().AddForce(
                 transform.forward * shootForce * Time.fixedDeltaTime);
@@ -93,11 +95,16 @@ public class Shoot : MonoBehaviour
     }
 
 
-       void OnCollisionEnter(Collision C){
-        if(C.gameObject.tag == "Projectile"){
-            UIManager.AddScore(20);
+    void OnCollisionEnter(Collision C){
+        if(C.gameObject.tag == "PowerUp_AutoShot" && CanPickUp != false){
             Destroy(C.gameObject);
+            UIManager.AddScore(10);
+            Invoke("CanPickUpAgain", 0.05f);
+            CanPickUp = false;
         }
     }
 
+    void CanPickUpAgain(){
+        CanPickUp = true;
+    }
 }
