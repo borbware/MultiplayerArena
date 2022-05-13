df41ed555b27bd3acda3c0241e183af24ab79215
Noora Heinsuo <noora.heinsuo@gmail.com>
2022-05-13 13:35:23 +0300

    Add Angles to cannon
    Cannon can be controlled up/down when used

--- Assets/Scenes/GroupX/Scripts/control.cs
+++ Assets/Scenes/GroupX/Scripts/control.cs
@@ -32,7 +32,22 @@ public class control : MonoBehaviour
     {
         if (playerController)
         {
-            transform.Rotate(new Vector3(0, playerController.axisInput.x * 100 * Time.deltaTime, 0));
+            transform.Rotate(
+                new Vector3(0, playerController.axisInput.x * 100 * Time.deltaTime, 0),
+                Space.World
+            );
+
+            transform.Rotate(
+                new Vector3(
+                    playerController.axisInput.y * 100 * Time.deltaTime,
+                    0, 0), Space.Self
+            );
+
+            Vector3 currentRotation = transform.rotation.eulerAngles;
+            Debug.Log(currentRotation);
+            currentRotation.x = Mathf.Clamp(currentRotation.x, 340, 359);
+            transform.localRotation = Quaternion.Euler(currentRotation);
+
             player.transform.position = seat.transform.position;
 
             if (playerController.jumpInput)
