b760f855cd49723426ccd3f50c7c8b4f4603ab20
PasiKl <pasi.kleimola@gmail.com>
2022-05-13 12:21:22 +0300

    Add tiles turning red before disappearing.

--- Assets/Scenes/GroupX/Scripts/TileRemover.cs
+++ Assets/Scenes/GroupX/Scripts/TileRemover.cs
@@ -6,11 +6,15 @@ public class TileRemover : MonoBehaviour
 {
     float interval, reset;
 
+    List<GameObject> tiles;
+
 
     // Start is called before the first frame update
     void Start()
     {
         reset = 1.0f;
+
+        tiles = new List<GameObject>();
     }
 
     // Update is called once per frame
@@ -25,9 +29,26 @@ public class TileRemover : MonoBehaviour
             var tile = GameObject.Find("Cube (" + tileNum + ")");
 
             if(tile != null && StageManager.instance.stageState == StageManager.StageState.Play)
-                tile.SetActive(false);
+            {
+                // tile.SetActive(false);
+
+                List<Material> mat = new List<Material>();
+                tile.GetComponent<MeshRenderer>().GetMaterials(mat);
+                mat[0].SetColor("_Color", Color.red);
+
+                tiles.Add(tile);
+
+                Invoke("DestroyTile", 3.0f);
+            }
 
             interval = reset;
         }
+    }
+
+    void DestroyTile()
+    {
+        tiles[0].SetActive(false);
+
+        tiles.Remove(tiles[0]);
     }
 }
