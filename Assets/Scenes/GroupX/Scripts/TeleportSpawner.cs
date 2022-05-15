762aa71e7decb99b795bc84be14a2da95d4968ff
PasiKl <pasi.kleimola@gmail.com>
2022-05-15 17:17:16 +0300

    Fix teleports. Teleports appear in pairs and disappear after a few seconds.

--- Assets/Scenes/GroupX/Scripts/TeleportSpawner.cs
+++ Assets/Scenes/GroupX/Scripts/TeleportSpawner.cs
@@ -7,16 +7,28 @@ public class TeleportSpawner : MonoBehaviour
     public GameObject teleportPrefab;
 
     private float timer = 0;
+
+
     void Update()
     {
         timer -= Time.deltaTime;
         if (timer <= 0 && StageManager.instance.stageState == StageManager.StageState.Play)
         {
-            Instantiate(teleportPrefab, new Vector3(
+            GameObject t1 = Instantiate(teleportPrefab, new Vector3(
                 Random.Range(3.5f, -3.5f),
                 0.5f,
                 Random.Range(3.5f, -3.5f)
             ), Quaternion.Euler(0, 0, 0));
+
+            t1.transform.Find("Model").tag = "Port1";
+
+            var t2 = Instantiate(teleportPrefab, new Vector3(
+                Random.Range(3.5f, -3.5f),
+                0.5f,
+                Random.Range(3.5f, -3.5f)
+            ), Quaternion.Euler(0, 0, 0));
+
+            t2.transform.Find("Model").tag = "Port2";
 
             timer = Random.Range(3, 8);
         }
