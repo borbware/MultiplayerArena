6fbb99e5c2396a749c2c9e7b0655c580142461ac
jptibirosen <11poiuy11@gmail.com>
2023-05-04 13:05:45 +0300

    fix some errors

--- Assets/Scenes/GroupX/Scripts/MoleScript.cs
+++ Assets/Scenes/GroupX/Scripts/MoleScript.cs
@@ -16,9 +16,11 @@ public class MoleScript : MonoBehaviour
 
     private void OnDestroy() {
         // we set call the MoleSpawner script to set the hole the mole was in to empty
-        GameObject.Find("RunningScripts").GetComponent<MoleSpawner>()
-        .listOfHoles[iAmInHoleNo].setEmpty();
-        //Debug.Log($"hole no {iAmInHoleNo} is empty");
+        if (iAmInHoleNo < GameObject.Find("RunningScripts").GetComponent<MoleSpawner>().listOfHoles.Count - 1){
+            GameObject.Find("RunningScripts").GetComponent<MoleSpawner>()
+            .listOfHoles[iAmInHoleNo].setEmpty();
+            //Debug.Log($"hole no {iAmInHoleNo} is empty");
+        }
     }
 
     private void OnTriggerEnter(Collider other) {
