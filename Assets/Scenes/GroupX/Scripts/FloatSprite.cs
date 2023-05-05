bdb0f5acdbe28c7965ae51d5108b1811e8322f99
ankeahko <hannes.hokkanen@gmail.com>
2023-05-05 20:46:30 +0300

    Ui tweaks and Float sprite script for Player icon.
    Also Skybox tweaks

---
+++ Assets/Scenes/GroupX/Scripts/FloatSprite.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class FloatSprite : MonoBehaviour {
+    public float amplitude = 0.1f;
+    public float speed = 1f;
+
+    private Vector3 startPosition;
+
+    void Start() {
+        startPosition = transform.position;
+    }
+
+    void Update() {
+        float yOffset = Mathf.Sin(Time.time * speed) * amplitude;
+        transform.position = startPosition + new Vector3(0f, yOffset, 0f);
+    }
+}
